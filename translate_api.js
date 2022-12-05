3b1f4f0954a27bfe404d31a2c35c36fa43bc3f0f
Edip Can Görgülü <edipcan98@hotmail.com>
2022-12-05 18:39:38 +0300

    first commit

---
+++ translate_api.js
@@ -0,0 +1,26 @@
+class TranslateApi {
+    constructor(ingilizceSaka) {
+        this.baseURL = 'https://translation.googleapis.com/';
+        this.aranacakCumle = ingilizceSaka;
+        this.axiosNesnesi = axios.create({
+            baseURL: this.baseURL,
+            params: {
+                target: 'tr',
+                key: 'AIzaSyASBa2VnNrdrNmAg5TXqcMY6VLUEYS4gnI',
+                q: this.aranacakCumle
+            }
+        });
+    }
+
+    async ceviriYap() {
+        try {
+            const ceviri = await this.axiosNesnesi.get('language/translate/v2');
+            console.log(ceviri.data.data.translations[0].translatedText);
+
+            return ceviri.data.data.translations[0].translatedText;
+        } catch (err) {
+            console.log(err.response);
+        }
+
+    }
+}
---
+++ unsplash_api.js
@@ -0,0 +1,26 @@
+class UnsplashApi {
+    constructor() {
+        this.baseURL = 'https://api.unsplash.com';
+        this.clientID = 'Client-ID OLtAwYq-1J4_o0alQnjn6bR_kzu57_-As4B2FXM1vNU';
+        this.axiosNesne = axios.create({
+            baseURL: this.baseURL,
+            headers: {
+                Authorization: this.clientID
+            },
+            paramms: {
+                query: 'animal',
+                count: 1
+            }
+        });
+    }
+    async randomResimGetir() {
+        try {
+            const resimResponse = await this.axiosNesne.get('/photos/random');
+            console.log(resimResponse.data[0].urls.regular);
+            return resimResponse.data[0].urls.regular;
+        } catch (err) {
+            console.log(err.response);
+            return 'https://picsum.photos/200/300';
+        }
+    }
+}
